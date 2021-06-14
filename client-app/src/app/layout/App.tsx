a7e80b7a86c2e545284721c9d06409895a2e9435
trevor <tmbarker9@gmail.com>
2021-06-13 19:30:36 -0700

    Add preliminary client routing (using React Router)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './loadingComponents';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 // NOTE: Use npm start to run this client app
 
 function App() {
-  const {activityStore} = useStore();
-
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
-
   return (
     // Implicit Fragment block
     <>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/activities' component={ActivityDashboard}/>
+        <Route path='/createActivity' component={ActivityForm}/>
       </Container>
     </>
   );
