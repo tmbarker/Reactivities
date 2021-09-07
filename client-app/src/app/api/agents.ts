a8bf1895ed333b750530d73882643ce68a900b98
trevor <tmbarker9@gmail.com>
2021-09-06 22:14:33 -0700

    FE for Attendance feature

--- client-app/src/app/api/agents.ts
+++ client-app/src/app/api/agents.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
-import { Activity } from '../Models/activity';
+import { Activity, ActivityFormValues } from '../Models/activity';
 import { User, UserFormValues } from '../Models/user';
 import { store } from '../stores/store';
 
@@ -62,15 +62,16 @@ const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
     post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
     put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    delete: <T> (url: string) => axios.delete<T>(url).then(responseBody),
+    del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id:string) => axios.delete<void>(`/activities/${id}`),
+    create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
+    update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id:string) => requests.del<void>(`/activities/${id}`),
+    attend: (id:string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
