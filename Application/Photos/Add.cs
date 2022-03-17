f782b22474116e82ba305daec339ca41b8fe3da2
trevor <tmbarker9@gmail.com>
2022-03-16 21:26:55 -0700

    Add endpoints for photo upload and delete

---
+++ Application/Photos/Add.cs
@@ -0,0 +1,69 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Photos
+{
+    public class Add
+    {
+        public class Command : IRequest<Result<Photo>>
+        {
+            public IFormFile File { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Photo>>
+        {
+            private readonly DataContext _context;
+            private readonly IPhotoAccessor _photoAccessor;
+            private readonly IUserNameAccessor _userNameAccessor;
+
+            public Handler(DataContext context, IPhotoAccessor photoAccessor, IUserNameAccessor userNameAccessor)
+            {
+                _context = context;
+                _photoAccessor = photoAccessor;
+                _userNameAccessor = userNameAccessor;
+            }
+
+            public async Task<Result<Photo>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Include(u => u.Photos)
+                    .FirstOrDefaultAsync(p => p.UserName == _userNameAccessor.GetUserName());
+
+                if (user == null)
+                {
+                    return null;
+                }
+
+                var photoUploadResult = await _photoAccessor.AddPhoto(request.File);
+                var photo = new Photo
+                {
+                    Id = photoUploadResult.PublicId,
+                    Url = photoUploadResult.Url,
+                };
+
+                if (!user.Photos.Any(p => p.IsMain))
+                {
+                    photo.IsMain = true;
+                }
+
+                user.Photos.Add(photo);
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (result)
+                {
+                    return Result<Photo>.Success(photo);
+                }
+
+                return Result<Photo>.Failure("Problem adding photo");
+            }
+        }
+    }
+}
